66123a6d4967a47730963405acd6de3310891cf1
yuanlongtao <yuanlongtao@u51.com>
2018-06-12 12:04:46 +0800

    mast修改

--- src/pattern/Decorator/sss.java
+++ src/pattern/Decorator/sss.java
@@ -9,5 +9,6 @@ package pattern.Decorator;
 public class sss {
     public static void main(String[] args) {
         System.out.println("---------------");
+        System.out.println("+++++++++++++++");
     }
 }
