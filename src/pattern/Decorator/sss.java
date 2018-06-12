4192ae9feed36537f7658f28c869ffbfb773cb4e
yuanlongtao <yuanlongtao@u51.com>
2018-06-12 12:00:40 +0800

    master!!!!!

---
+++ src/pattern/Decorator/sss.java
@@ -0,0 +1,13 @@
+package pattern.Decorator;
+
+/**
+ * @Auther: yuanlongtao
+ * @Date: 2018/6/12 12:00
+ * @Description:
+ * @Version: 1.0
+ */
+public class sss {
+    public static void main(String[] args) {
+        System.out.println("---------------");
+    }
+}
